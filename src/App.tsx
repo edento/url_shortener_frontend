0b8c3fd168bfe75222432e30f1e1c9b1ee45ceac
IsraelKariti <israelkariti@gmail.com>
2023-04-17 22:31:17 +0300

    arrange requires

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import Button from "./components/Button";
 import { useState } from "react";
 
-
-
 function App() {
   const [urlLegal, setUrlLegality] = useState(false);
-  const [message, setMessage] = useState('');
-  const [shortenedUrl, setShortenedUrl] = useState('')
+  const [url, setUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
 
   function handleChange(value: string) {
     const regEx = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
     if (value.match(regEx)) {
       setUrlLegality(true);
-      setMessage(value);
+      setUrl(value);
     }
     else {
       setUrlLegality(false);
-      setMessage('');
+      setUrl('');
     }
   }
 
@@ -28,8 +26,8 @@ function App() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title: 'React POST Request Example' })
     };
-    console.log('to fetch : '+'http://localhost:3000/'+message)
-    fetch('http://localhost:3000/'+message, requestOptions)
+    console.log('to fetch : '+'http://localhost:3000/'+url)
+    fetch('http://localhost:3000/'+url, requestOptions)
     .then((response) => response.json())
     .then((responseJson)=>{setShortenedUrl(responseJson.short)})
     .catch(()=>{
